0fa8547f6c54f73b5d22e547ee90386b5037624a
Ramon <84084404+Ramon-Erik@users.noreply.github.com>
2024-06-18 21:04:34 -0300

    Update geral.php

--- view/resultado/geral.php
+++ view/resultado/geral.php
@@ -4,12 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resultado geral</title>
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-    <?php 
-    require_once '../../model/Votacao.Class.php';
-    $resultado = new Votacao;
-    $resultado->resultado_geral();
-    ?>
+    <header></header>
+    <main>
+        <article>
+            <h1>Resultado geral</h1>
+        </article>
+        <div class="container-tabela">
+            <?php
+            require_once '../../model/Votacao.Class.php';
+            $resultado = new Votacao;
+            $resultado->resultado_geral_m();
+            ?>
+        </div>
+        <div class="container-tabela">
+            <?php
+            require_once '../../model/Votacao.Class.php';
+            $resultado = new Votacao;
+            $resultado->resultado_geral_f();
+            ?>
+        </div>
+    </main>
 </body>
 </html>
